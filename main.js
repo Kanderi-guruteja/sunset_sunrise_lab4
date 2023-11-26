129934ca474a11e33e95ed2958c67e6c44f0a1cd
Guruteja Kanderi <105558277+Kanderi-guruteja@users.noreply.github.com>
2023-11-26 11:58:10 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,19 +2,20 @@ function searchLocation() {
   const locationInput = document.getElementById('location');
   const location = locationInput.value;
 
-  // Use the geocode API to get latitude and longitude
-  const geocodeUrl = `https://geocode.maps.co/search?q=${encodeURIComponent(location)}`;
+  // Replace 'YOUR_OPENCAGE_API_KEY' with your actual OpenCage API key
+  const geocodeApiKey = 'YOUR_OPENCAGE_API_KEY';
+  const geocodeUrl = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(location)}&key=${geocodeApiKey}`;
 
   fetch(geocodeUrl)
     .then(response => response.json())
     .then(data => {
       if (data.results && data.results.length > 0) {
-        const latitude = data.results[0].lat;
-        const longitude = data.results[0].lon;
+        const latitude = data.results[0].geometry.lat;
+        const longitude = data.results[0].geometry.lng;
 
         // Replace 'YOUR_SUNRISE_SUNSET_API_KEY' with your actual API key
-        const apiKey = 'YOUR_SUNRISE_SUNSET_API_KEY';
-        const sunriseSunsetUrl = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=today&formatted=0&apiKey=${apiKey}`;
+        const sunriseSunsetApiKey = 'YOUR_SUNRISE_SUNSET_API_KEY';
+        const sunriseSunsetUrl = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=today&formatted=0&key=${sunriseSunsetApiKey}`;
 
         fetch(sunriseSunsetUrl)
           .then(response => response.json())
