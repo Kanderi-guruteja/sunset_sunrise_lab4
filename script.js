b25dc832b36074d0b994e7889b19a279dfa07614
Guruteja Kanderi <105558277+Kanderi-guruteja@users.noreply.github.com>
2023-11-27 19:02:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 const API_URL = 'https://api.sunrisesunset.io/json';
 
 function getLocationCoordinates(address) {
-  const params = {
-    q: address,
-    format: 'json'
-  };
+  if (address) {
+    // Forward geocoding for specified address
+    const params = {
+      q: address,
+      format: 'json'
+    };
 
-  const url = new URL('https://geocode.maps.co/search');
-  url.search = new URLSearchParams(params).toString();
+    const url = new URL('https://geocode.maps.co/search');
+    url.search = new URLSearchParams(params).toString();
 
-  return fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      if (data.status === 'OK') {
-        const results = data.results[0];
-        const latitude = results.latitude;
-        const longitude = results.longitude;
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'OK') {
+          const results = data.results[0];
+          const latitude = results.latitude;
+          const longitude = results.longitude;
 
-        return { latitude, longitude };
-      } else {
-        throw new Error('Geocoding failed');
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching coordinates:', error);
-    });
+          return { latitude, longitude };
+        } else {
+          throw new Error('Geocoding failed');
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching coordinates:', error);
+      });
+  } else {
+    // Reverse geocoding for current location
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        getSunriseSunsetData({ latitude, longitude });
+      }, error => {
+        console.error('Error getting current location:', error);
+      });
+    } else {
+      console.error('Geolocation not supported by this browser');
+    }
+  }
 }
 
 function getSunriseSunsetData(coordinates) {
@@ -74,10 +91,12 @@ function initialize() {
   const searchBtn = document.getElementById('search-btn');
   searchBtn.addEventListener('click', () => {
     const locationInput = document.getElementById('location').value;
-    if (locationInput) {
-      getLocationCoordinates(locationInput)
-        .then(coordinates => getSunriseSunsetData(coordinates));
-    }
+    getLocationCoordinates(locationInput);
+  });
+
+  const geolocationBtn = document.getElementById('geolocation-btn');
+  geolocationBtn.addEventListener('click', () => {
+    getLocationCoordinates();
   });
 }
 
